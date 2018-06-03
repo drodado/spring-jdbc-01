11e5646d95047c936284f944d1059318dd5a3656
David Rodado Fernández <david.rodado@asseco.es>
2018-06-03 23:42:38 +0200

    First project commit

---
+++ src/main/resources/datos.sql
@@ -0,0 +1,2 @@
+insert into persona (nombre, ape_paterno, email) values ('Admin', 'admin', 'admin@icursos.net');
+insert into persona (nombre, ape_paterno, email) values ('Juan', 'Perez', 'juan@gmail.com.com');
